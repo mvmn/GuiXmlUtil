dbf50b4e8ccdea91fd503f54fff48d25b542fb9f
mvmn <sauron.inbox@gmail.com>
2013-09-09 01:01:12 +0300

    Added namespaces support to XPath evaluator. Updated layout. Removed XmlPanel.

--- src/x/mvmn/xmlworks/XmlWorks.java
+++ src/x/mvmn/xmlworks/XmlWorks.java
@@ -2,12 +2,12 @@ package x.mvmn.xmlworks;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -21,11 +21,13 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -46,14 +48,40 @@ public class XmlWorks implements ActionListener {
 	private final JTextArea txaXsl;
 	private final JTextArea txaResult;
 	private final JTable tblParams;
+	private final JTable tblNamespaces;
 	private final JButton btnSetParam;
 	private final JButton btnRemoveParam;
 	private final JButton btnRunXsl;
 	private final JTextField tfXpath;
 	private final JButton btnRunXpath;
+	private final JButton btnAddNamespace;
+	private final JButton btnRemoveNamespace;
 
 	private final Map<String, String> xslParams = new TreeMap<String, String>();
 	private final List<String> xslParamsList = new ArrayList<String>();
+	private final JTabbedPane tbpProcessing;
+
+	private final Map<String, String> xpathNamespacesPrefixToUri = new TreeMap<String, String>();
+	private final Map<String, String> xpathNamespacesUriToPrefix = new TreeMap<String, String>();
+	private final List<String> xpathNamespacePrefixes = new ArrayList<String>();
+
+	private final NamespaceContext namespaceContext = new NamespaceContext() {
+
+		@Override
+		public Iterator<?> getPrefixes(String namespaceURI) {
+			return xpathNamespacePrefixes.iterator();
+		}
+
+		@Override
+		public String getPrefix(String namespaceURI) {
+			return xpathNamespacesUriToPrefix.get(namespaceURI);
+		}
+
+		@Override
+		public String getNamespaceURI(String prefix) {
+			return xpathNamespacesPrefixToUri.get(prefix);
+		}
+	};
 
 	public XmlWorks() {
 		xslParams.put("testParam", "Param Value");
@@ -85,65 +113,106 @@ public class XmlWorks implements ActionListener {
 			}
 		};
 
+		TableModel namespacesTableModel = new AbstractTableModel() {
+			private static final long serialVersionUID = 7639530381489168188L;
+
+			@Override
+			public Object getValueAt(int rowIndex, int columnIndex) {
+				Object result = null;
+				if (rowIndex < xpathNamespacePrefixes.size()) {
+					result = xpathNamespacePrefixes.get(rowIndex);
+					if (result != null && columnIndex > 0) {
+						result = xpathNamespacesPrefixToUri.get(result);
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public int getRowCount() {
+				return xpathNamespacePrefixes.size();
+			}
+
+			@Override
+			public int getColumnCount() {
+				return 2;
+			}
+		};
+
+		tbpProcessing = new JTabbedPane();
+
 		btnSetParam = new JButton("Set parameter");
 		btnRemoveParam = new JButton("Remove parameter");
 		btnRunXsl = new JButton("Run XSL Transformation");
 		btnRunXpath = new JButton("Evaluate XPath expression");
 
+		btnAddNamespace = new JButton("Add namespace");
+		btnRemoveNamespace = new JButton("Remove namespace");
+
 		btnSetParam.addActionListener(this);
 		btnRemoveParam.addActionListener(this);
 		btnRunXsl.addActionListener(this);
 		btnRunXpath.addActionListener(this);
+		btnAddNamespace.addActionListener(this);
+		btnRemoveNamespace.addActionListener(this);
+
+		tfXpath = new JTextField("/root//test:*[contains(name(),'lem')]");
+		xpathNamespacePrefixes.add("test");
+		xpathNamespacesPrefixToUri.put("test", "test:namespace");
+		xpathNamespacesUriToPrefix.put("test:namespace", "test");
+		tblNamespaces = new JTable(namespacesTableModel);
+		JPanel namespacesPanel = new JPanel(new BorderLayout());
+		namespacesPanel.add(btnAddNamespace, BorderLayout.NORTH);
+		namespacesPanel.add(new JScrollPane(tblNamespaces), BorderLayout.CENTER);
+		namespacesPanel.add(btnRemoveNamespace, BorderLayout.SOUTH);
+		JPanel xpathPanel = new JPanel(new BorderLayout());
+		{
+			JPanel xpathFieldPanel = new JPanel(new BorderLayout());
+			xpathFieldPanel.add(new JLabel("XPath expression:"), BorderLayout.WEST);
+			xpathFieldPanel.add(tfXpath, BorderLayout.CENTER);
+			xpathFieldPanel.setBorder(BorderFactory.createEmptyBorder(2, 6, 2, 2));
+			xpathPanel.add(xpathFieldPanel, BorderLayout.NORTH);
+		}
+		xpathPanel.add(namespacesPanel, BorderLayout.CENTER);
+		xpathPanel.add(btnRunXpath, BorderLayout.SOUTH);
 
 		mainWindow = new JFrame("XmlWorks");
 		mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		txaXml = new JTextArea(
-				"<root>\n\t<elem attrOne=\"at1val\" attrTwo=\"at2val\">\n\t\tSome text\n\t</elem>\n\t<otherElem at3=\"three\">\n\t</otherElem>\n</root>\n");
+				"<root>\n\t<test:elem attrOne=\"at1val\" attrTwo=\"at2val\" xmlns:test=\"test:namespace\">\n\t\tSome text\n\t\t<test:subElem>Some other text</test:subElem>\n\t</test:elem>\n\t<otherElem at3=\"three\">\n\t</otherElem>\n</root>\n");
 		txaXml.setTabSize(3);
-		txaXsl = new JTextArea("<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n" + "\t<xsl:param name=\"testParam\"/>\n"
-				+ "\t<xsl:template match=\"/root/elem\">\n" + "\t\t<xsl:element name=\"newElem\">\n" + "\t\t\t<xsl:attribute name=\"newAttr\">\n"
-				+ "\t\t\t\t<xsl:value-of select=\"@attrOne\" />\n" + "\t\t\t\t<xsl:value-of select=\"'/'\" />\n"
-				+ "\t\t\t\t<xsl:value-of select=\"$testParam\" />\n" + "\t\t\t</xsl:attribute>\n" + "\t\t</xsl:element>\n" + "\t</xsl:template>\n"
-				+ "</xsl:stylesheet>");
+		txaXsl = new JTextArea(
+				"<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"  xmlns:test=\"test:namespace\">\n\t<xsl:param name=\"testParam\"/>\n\n\t<xsl:template match=\"/root/test:elem\">\n\t\t<xsl:element name=\"newElem\">\n\t\t\t<xsl:attribute name=\"newAttr\">\n\t\t\t\t<xsl:value-of select=\"@attrOne\" />\n\t\t\t\t<xsl:value-of select=\"'/'\" />\n\t\t\t\t<xsl:value-of select=\"$testParam\" />\n\t\t\t</xsl:attribute>\n\t\t</xsl:element>\n\t</xsl:template>\n</xsl:stylesheet>\n");
 		txaXsl.setTabSize(3);
 		txaResult = new JTextArea();
 		txaResult.setTabSize(3);
-
-		tfXpath = new JTextField("/root/*[contains(name(),'lem')]");
 
 		tblParams = new JTable(paramsTableModel);
 		JPanel xslParamsPanel = new JPanel(new BorderLayout());
 		xslParamsPanel.add(btnSetParam, BorderLayout.NORTH);
 		xslParamsPanel.add(new JScrollPane(tblParams), BorderLayout.CENTER);
 		xslParamsPanel.add(btnRemoveParam, BorderLayout.SOUTH);
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, false, new JScrollPane(txaXml), new JScrollPane(txaXsl));
-		splitPane.setDividerLocation(0.5);
-		splitPane.setResizeWeight(0.5);
+
+		JSplitPane splitPaneXsl = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, false, new JScrollPane(txaXsl), xslParamsPanel);
+		tbpProcessing.addTab("XSL Transformation", splitPaneXsl);
+		tbpProcessing.addTab("XPath", xpathPanel);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, false, new JScrollPane(txaXml), tbpProcessing);
 		JSplitPane splitPaneTwo = new JSplitPane(JSplitPane.VERTICAL_SPLIT, false, splitPane, new JScrollPane(txaResult));
-		splitPaneTwo.setDividerLocation(0.5);
-		splitPaneTwo.setResizeWeight(0.5);
+
 		Container contentPane = mainWindow.getContentPane();
 		contentPane.setLayout(new BorderLayout());
-		JSplitPane splitPaneThree = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, false, splitPaneTwo, xslParamsPanel);
-		{
-			JPanel xpathPanel = new JPanel(new BorderLayout());
-			xpathPanel.add(new JLabel("XPath expression:"), BorderLayout.WEST);
-			xpathPanel.add(tfXpath, BorderLayout.CENTER);
-			xpathPanel.setBorder(BorderFactory.createEmptyBorder(2, 6, 2, 2));
-			contentPane.add(xpathPanel, BorderLayout.NORTH);
-		}
-		contentPane.add(splitPaneThree, BorderLayout.CENTER);
-		{
-			JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
-			buttonPanel.add(btnRunXsl);
-			buttonPanel.add(btnRunXpath);
-			contentPane.add(buttonPanel, BorderLayout.SOUTH);
-		}
+		contentPane.add(splitPaneTwo, BorderLayout.CENTER);
+		contentPane.add(btnRunXsl, BorderLayout.SOUTH);
 
+		splitPaneTwo.setResizeWeight(0.7);
+		splitPaneXsl.setResizeWeight(0.5);
+		splitPane.setResizeWeight(0.5);
 		mainWindow.pack();
-		splitPaneThree.setDividerLocation(0.7);
-		splitPaneThree.setResizeWeight(0.7);
+		splitPaneTwo.setDividerLocation(0.7);
+		splitPane.setDividerLocation(0.5);
+		splitPaneXsl.setDividerLocation(0.5);
 	}
 
 	public static void main(String[] args) {
@@ -170,9 +239,34 @@ public class XmlWorks implements ActionListener {
 		} else if (actEvent.getSource() == btnSetParam) {
 			XPathFactory factory = XPathFactory.newInstance();
 			XPath xPath = factory.newXPath();
+			xPath.setNamespaceContext(new NamespaceContext() {
+
+				private List<String> contexts;
+
+				{
+					contexts = new ArrayList<String>(1);
+					contexts.add("xsl");
+				}
+
+				@Override
+				public Iterator<?> getPrefixes(String namespaceURI) {
+					return contexts.iterator();
+				}
+
+				@Override
+				public String getPrefix(String namespaceURI) {
+					return namespaceURI.equals("http://www.w3.org/1999/XSL/Transform") ? "xsl" : null;
+				}
+
+				@Override
+				public String getNamespaceURI(String prefix) {
+					return prefix.equals("xsl") ? "http://www.w3.org/1999/XSL/Transform" : null;
+				}
+			});
+
 			List<String> options = new LinkedList<String>();
 			try {
-				XPathExpression expr = xPath.compile("/*[name()='xsl:stylesheet']/*[name()='xsl:param']/@name");
+				XPathExpression expr = xPath.compile("/xsl:stylesheet/xsl:param/@name");
 				Object result = expr.evaluate(new InputSource(new StringReader(txaXsl.getText())), XPathConstants.NODESET);
 
 				NodeList nodes = (NodeList) result;
@@ -218,6 +312,7 @@ public class XmlWorks implements ActionListener {
 			try {
 				XPathFactory factory = XPathFactory.newInstance();
 				XPath xPath = factory.newXPath();
+				xPath.setNamespaceContext(namespaceContext);
 				XPathExpression expr = xPath.compile(tfXpath.getText());
 				Object result = expr.evaluate(new InputSource(new StringReader(txaXml.getText())), XPathConstants.NODESET);
 
@@ -232,6 +327,46 @@ public class XmlWorks implements ActionListener {
 					resultText.append(sw.toString()).append("\n");
 				}
 				txaResult.setText(resultText.toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(mainWindow, "Exception occurred: " + e.getClass().getName() + " - " + e.getMessage());
+			}
+		} else if (actEvent.getSource() == btnRemoveNamespace) {
+			int selectedRow = tblNamespaces.getSelectedRow();
+			if (selectedRow >= 0) {
+				String key = (String) tblNamespaces.getModel().getValueAt(selectedRow, 0);
+				String uri = xpathNamespacesPrefixToUri.remove(key);
+				xpathNamespacesUriToPrefix.remove(uri);
+				xpathNamespacePrefixes.remove(key);
+				tblNamespaces.invalidate();
+				tblNamespaces.revalidate();
+				tblNamespaces.repaint();
+			}
+		} else if (actEvent.getSource() == btnAddNamespace) {
+			try {
+				String namespacePrefix = JOptionPane.showInputDialog(mainWindow, "Enter namespace prefix", "XML namespace", JOptionPane.PLAIN_MESSAGE);
+
+				if (namespacePrefix != null && namespacePrefix.trim().length() > 0) {
+					String namespaceUri = JOptionPane.showInputDialog(mainWindow, "Enter namespace URI");
+					if (namespaceUri != null && namespaceUri.trim().length() > 0) {
+						namespacePrefix = namespacePrefix.trim();
+						namespaceUri = namespaceUri.trim();
+
+						if (xpathNamespacePrefixes.contains(namespacePrefix)) {
+							xpathNamespacesUriToPrefix.remove(xpathNamespacesPrefixToUri.get(namespacePrefix));
+							xpathNamespacesPrefixToUri.put(namespacePrefix, namespaceUri);
+							xpathNamespacesUriToPrefix.put(namespaceUri, namespacePrefix);
+						} else {
+							xpathNamespacesPrefixToUri.put(namespacePrefix, namespaceUri);
+							xpathNamespacesUriToPrefix.put(namespaceUri, namespacePrefix);
+							xpathNamespacePrefixes.add(namespacePrefix);
+						}
+
+						tblNamespaces.invalidate();
+						tblNamespaces.revalidate();
+						tblNamespaces.repaint();
+					}
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 				JOptionPane.showMessageDialog(mainWindow, "Exception occurred: " + e.getClass().getName() + " - " + e.getMessage());
